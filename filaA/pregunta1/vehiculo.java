623436fb79684f6b8160e4ad11997d383c64bf7e
ignacio lizarazu aramayo <lizarazuaramayoignacio@gmail.com>
2022-05-27 08:21:36 -0400

    parcial 1 commite 2

---
+++ filaA/pregunta1/vehiculo.java
@@ -0,0 +1,28 @@
+ package primeParcialPatrones.pregunta1
+ public clas vehiculo{
+ private String placa;
+ private String modelo;
+ private String color;
+
+ public vehiculo(){}
+ public vehiulo(String placa, String modelo , String color){
+ this.placa = placa;
+ this.modelo= modelo;
+ this.color =colo;
+ }
+ public String getPlaca(){ return placa;}
+ public void setPlaca(String placa){this.placa = placa}
+ public String getModelo(){return modelo;}
+ public void setModelo(String modelo) {this.modelo = modelo;}
+ public String getColor(){return color;}
+ public void setColor(String color){this.color = color}
+
+ public void showVehiculo(){
+ system.out.println("************************")
+ system.out.println("placa"+placa);
+ system.out.println("modelo"+modelo);
+ system.out.println("color"+color);
+
+
+ }
+ }
